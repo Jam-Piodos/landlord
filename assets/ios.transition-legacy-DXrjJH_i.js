d4b35b0cabe24732f2fefc295bc094926b70c096
Jam-Piodos <20221071@nbsc.edu.ph>
2025-07-09 14:44:13 +0800

    Updates

--- assets/ios.transition-legacy-DXrjJH_i.js
+++ assets/ios.transition-legacy-DXrjJH_i.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DxPVT80b.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BA0CL6RU.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-DG3bef5u.js
+++ assets/swipe-back-legacy-DG3bef5u.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DxPVT80b.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-BA0CL6RU.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
